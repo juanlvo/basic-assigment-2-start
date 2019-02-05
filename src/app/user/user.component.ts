d7375efe90bd1b2759206a7af8a370235939a7a7
Juan Vivas <juanlvo@gmail.com>
2019-02-05 09:31:12 +0100

    feat: solution of the assignment

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  username = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onUpdateUsername(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  emptyUsername() {
+    if (this.username == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  setNullUsername() {
+    this.username = null;
+  }
 }
